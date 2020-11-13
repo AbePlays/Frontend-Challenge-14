8617c3d788d226da0b7022cc85beabd6bcfc3586
Abe <abhi.rawat456@gmail.com>
2020-11-13 17:10:53 +0530

    Added Framer Motion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { AnimatePresence } from "framer-motion";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import CardDetails from "./components/CardDetails";
 import NotFound from "./components/NotFound";
 
 function App() {
+  let location = useLocation();
   const isDark = useSelector((state) => state.darkMode);
 
   return (
-    <Router>
-      <div className={`min-h-screen ${isDark ? "bg-gray-900" : "bg-white"}`}>
-        <Header />
-        <Switch>
+    <div className={`min-h-screen ${isDark ? "bg-gray-900" : "bg-white"}`}>
+      <Header />
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.key}>
           <Route path="/Frontend-Challenge-14/" exact component={Body} />
           <Route
             path="/Frontend-Challenge-14/details"
@@ -20,8 +22,8 @@ function App() {
           />
           <Route component={NotFound} />
         </Switch>
-      </div>
-    </Router>
+      </AnimatePresence>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import allReducer from "./reducers/index";
@@ -10,7 +11,9 @@ let store = createStore(allReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
 
   document.getElementById("root")
